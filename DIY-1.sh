15c9d9b8fb4d16d39f7d5becee0e798c87f041c2
MIT-X <134132915+MIT-X@users.noreply.github.com>
2024-09-28 23:07:56 +0800

    Add files via upload

---
+++ DIY-1.sh
@@ -0,0 +1,11 @@
+# 此脚本用处是：添加第三方插件
+# =============================================================
+
+# 1-添加插件仓库
+# echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+# echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+
+echo 'https://github.com/kiddin9/openwrt-packages' >>feeds.conf.default
+
+# rm -rf feeds/packages/lang/golang
+# git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
