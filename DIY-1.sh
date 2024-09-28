195f70e2b492c454d9b2fb7ed5f91f9ad36c85a9
MIT-X <134132915+MIT-X@users.noreply.github.com>
2024-09-28 23:19:31 +0800

    Update DIY-1.sh

--- DIY-1.sh
+++ DIY-1.sh
@@ -5,7 +5,9 @@
 # echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 # echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 
-echo 'https://github.com/kiddin9/openwrt-packages' >>feeds.conf.default
+
+echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages' >>feeds.conf.default
+
 
 # rm -rf feeds/packages/lang/golang
 # git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
