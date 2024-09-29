a17fbc5c3611ce9580fe63b2657642aa00a6ff8d
MIT-X <134132915+MIT-X@users.noreply.github.com>
2024-09-29 17:29:13 +0800

    Update DIY-1.sh

--- DIY-1.sh
+++ DIY-1.sh
@@ -9,5 +9,5 @@ git clone https://github.com/jerrykuku/luci-theme-argon.git  package/luci-theme-
 git clone https://github.com/WukongMaster/luci-theme-teleofis.git package/luci-theme-teleofis
 
 # 4-添加 PassWall2 插件
-echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> "feeds.conf.default"
-echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+# echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> "feeds.conf.default"
+# echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
