2f74fbcc463d72e5087a33c1cdd33d0da2c221a7
MIT-X <134132915+MIT-X@users.noreply.github.com>
2024-09-29 14:16:22 +0800

    Update DIY-CS.sh

--- DIY-CS.sh
+++ DIY-CS.sh
@@ -17,7 +17,7 @@ git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-a
 git clone https://github.com/WukongMaster/luci-theme-teleofis.git package/luci-theme-teleofis
 
 
-# 5-添加 PassWall 插件
+# 5-添加 PassWall2 插件
 echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> "feeds.conf.default"
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
 
