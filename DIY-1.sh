6a97fc6a5114f807f2ff8a5f20eafbe9e047542c
MIT-X <134132915+MIT-X@users.noreply.github.com>
2024-09-29 16:04:12 +0800

    Update DIY-1.sh

--- DIY-1.sh
+++ DIY-1.sh
@@ -1,9 +1,6 @@
 # 此脚本用处是：添加第三方插件
 #==============================================================
 
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
-
 # 1-SSR插件
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 
@@ -17,4 +14,3 @@ git clone https://github.com/WukongMaster/luci-theme-teleofis.git package/luci-t
 # 4-添加 PassWall2 插件
 echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> "feeds.conf.default"
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
-
